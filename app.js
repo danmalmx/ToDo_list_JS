b3e74628b90a1ed90a7a788ce98de20eca70e6e7
danmalmx <danmalmx@gmail.com>
2019-06-03 20:08:41 +0200

    adding event listener and task adding function

--- app.js
+++ app.js
@@ -1,6 +1,43 @@
 // UI Variables
 const form = document.querySelector("#task-form");
 const taskList = document.querySelector(".collection");
-const clearBtn = document.querySelector(".clear-tasks");
-const filter = document.querySelector("#filter");
+const clearBtn = document.querySelector(".clear-task");
 const taskInput = document.querySelector("#task");
+const filter = document.querySelector("#filter");
+
+// Load event listeners
+function loadEventListeners() {}
+
+//Add task event
+form.addEventListener("submit", addTask);
+
+//Add task function
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("Add a task");
+  }
+  // Add task element
+  const li = document.createElement("li");
+  //Add class name
+  li.className = "collection-item";
+  //Adding text to li elelment
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  //Create link element
+  const link = document.createElement("a");
+  //Add class name
+  link.className = "delete-item secondary-content";
+  //Add icon (html)
+  link.innerHTML = '<i class="fa fa-remove"</i>';
+
+  //Append link to li
+  li.appendChild(link);
+
+  //Append li to ul
+  taskList.appendChild(li);
+
+  //Clear input
+  taskInput.value = "";
+
+  e.preventDefault();
+}
