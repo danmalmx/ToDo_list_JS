158e5bfe9c080e1254770e42a0bbbc53374b0555
danmalmx <danmalmx@gmail.com>
2019-06-04 07:53:38 +0200

    adding function to clear tasks from local storage

--- app.js
+++ app.js
@@ -102,8 +102,26 @@ function removeTask(e) {
   if (e.target.parentElement.classList.contains("delete-item")) {
     if (confirm("Are you sure?")) {
       e.target.parentElement.parentElement.remove();
+
+      //Remove from local storage
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
     }
   }
+}
+
+//Removing task from local storage
+function removeTaskFromLocalStorage(taskItem) {
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.forEach(function(task, index) {
+    if (taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 //Clear tasks button function
